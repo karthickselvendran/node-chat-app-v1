07371644070bfcf266f19242d826e933448ff4f1
karthickselvendran <selvendrankarthick@gmail.com>
2022-04-10 23:46:53 +0530

    get api added with jwt

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -7,8 +7,10 @@ const jwtAuth = async (req, res, next) => {
     const accessToken = req.get('Authorization');
     try {
         const userToken = jwt.verify(accessToken, jwtConstants.secretKey, { expiresIn: jwtConstants.exp_time });
+        // console.log("id", userToken._id)
         const userDetails = await users.findById(userToken._id)
         if (userDetails) {
+            // console.log(userDetails.toJSON())
             req.user = userDetails.toJSON();
         }
     } catch (err) {
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { getUsersList } = require('../controller/usersDatas');
 const router = express.Router();
-
+const { jwtAuth } = require('../middleware/userAuth')
 const { signup, signin } = require('../controller/Auth');
 
-//Auth
+// Auth
 router.post('/signup', signup)
 router.post('/signin', signin)
+
+// apis
+router.use('/*', jwtAuth);
+router.get('/getUsersList', getUsersList);
 
 module.exports = router;
