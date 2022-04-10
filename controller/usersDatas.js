ebd459656f79e1037c0ad66b8d2f299d8fdbb94a
karthickselvendran <selvendrankarthick@gmail.com>
2022-04-11 02:07:15 +0530

    code format changes

--- controller/usersDatas.js
+++ controller/usersDatas.js
@@ -1,13 +1,8 @@
 const Users = require('../schema/users');
 
 const getUsersList = async (req, res) => {
-    // console.log("req.user")
-    // console.log(req.user)
     try {
-        console.log("req.user._id")
-        console.log(req.user._id)
         const usersList = await Users.find({ _id: { $ne: req.user._id } }).select("-password")
-        console.log(usersList)
         return res.status(200).json({
             status: 200,
             data: usersList
